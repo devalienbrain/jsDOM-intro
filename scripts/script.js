61da7598debdef00958d643dd0e4b56b1a0a797d
Alien Brain <murad.dub@gmail.com>
2023-08-12 23:29:14 +0600

    event.html added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-console.log("hello from external js file!");
+
+// Finding HTML elements by tag name
+
+const specialTextElements = document.getElementsByClassName('special-text');
+
+for (element of specialTextElements) {
+  console.log(element.innerText);
+}
