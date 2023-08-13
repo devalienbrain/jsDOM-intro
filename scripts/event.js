bbfbf777f8236a911b3a168e23a6c3660bebca12
Alien Brain <murad.dub@gmail.com>
2023-08-13 21:01:11 +0600

    add comments section added

--- scripts/event.js
+++ scripts/event.js
@@ -8,3 +8,24 @@ function makeBlue() {
 document.getElementById('red-theme').addEventListener('click', function () {
   document.body.style.backgroundColor = 'red';
 })
+
+
+// secret message part
+const inputText = document.getElementById('input-text');
+
+const hideBtn = document.getElementById('hide-btn');
+
+const secretMessage = document.getElementById('secret-message');
+
+inputText.addEventListener('keyup', function () {
+  if (inputText.value === 'delete') {
+    hideBtn.disabled = false;
+  }
+  else {
+    hideBtn.disabled = true;
+  }
+});
+
+hideBtn.addEventListener('click', function () {
+  secretMessage.style.display = 'none';
+});
